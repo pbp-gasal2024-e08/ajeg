6c18e6dfcfd90b4dfb38c986d94a82a4794b8b1d
MNTYROS <mnadzimtahara@gmail.com>
2024-10-23 22:24:18 +0700

    added products empty to main, and routing for most checkout-history

---
+++ checkout/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from checkout.views import show_cart, show_checkout, show_order_summary, show_product_page
+
+app_name = 'checkout'
+
+urlpatterns = [
+    path('checkout/', show_checkout, name='show_checkout'),
+    path('cart/', show_cart, name='show_cart'),
+    path('order_summary/', show_order_summary, name='show_order_summary'),
+    path('product_page/<int:pk>/', show_product_page, name='show_product_page'),
+]
--- checkout/views.py
+++ checkout/views.py
@@ -1,3 +1,24 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
+from django.contrib.auth.context_processors import auth 
 
+from main.models import Product
 # Create your views here.
+
+
+@login_required
+def show_cart(request):
+    return render(request, 'cart.html')
+
+
+@login_required
+def show_checkout(request):
+    return render(request, 'checkout.html')
+
+@login_required
+def show_order_summary(request):
+    return render(request, 'order_summary.html')
+
+def show_product_page(request, pk):
+    product = Product.objects.get(pk=pk)
+    return render(request, 'product.html', {'product': product})
