b331570f1d5dd3d3cfba472ae6661ef1ba1615e6
tobtoby <tobyas2005139@gmail.com>
2024-10-24 11:37:58 +0700

    Created panel to house reviews and views to fetch reviews (+ routing)

--- review/urls.py
+++ review/urls.py
@@ -3,7 +3,16 @@ from django.urls import path
 from review.views import *
 
 urlpatterns = [
+    path("product/<int:product_id>", render_reviews_panel, name="render_reviews_panel"),
     path("", fetch_all_reviews, name="fetch_all_reviews"),
-    path("<uuid:id>", fetch_review, name="fetch_review"),
+    path("<uuid:id>", fetch_review_by_id, name="fetch_review"),
+    path(
+        "by_product/<int:product_id>",
+        fetch_reviews_by_product,
+        name="fetch_reviews_by_product",
+    ),
+    path(
+        "by_store/<int:store_id>", fetch_reviews_by_store, name="fetch_reviews_by_store"
+    ),
     path("add_comment/<uuid:id>", add_comment, name="add_comment"),
 ]
--- review/views.py
+++ review/views.py
@@ -3,13 +3,17 @@ from django.http import HttpResponse, HttpRequest
 from django.contrib.auth.decorators import login_required
 from django.views.decorators.http import require_POST
 
-
 from review.models import *
+from main.models import Product
 from django.core.serializers import serialize
 
 
-def fetch_review(request: HttpRequest, id: uuid.UUID) -> HttpResponse:
-    review: UserReview = None
+def render_reviews_panel(request, product_id: int):
+    product = Product.objects.get(pk=product_id)
+    return render(request, "reviews_panel.html", {"product": product})
+
+
+def fetch_review_by_id(request: HttpRequest, id: uuid.UUID) -> HttpResponse:
     response = HttpResponse()
 
     try:
@@ -24,10 +28,26 @@ def fetch_review(request: HttpRequest, id: uuid.UUID) -> HttpResponse:
     return response
 
 
+def fetch_reviews_by_product(request: HttpRequest, product_id: int):
+    try:
+        product = Product.objects.get(pk=product_id)
+        reviews = UserReview.objects.filter(product=product)
+    except Product.DoesNotExist or UserReview.DoesNotExist:
+        return HttpResponse("Product not found", status=404)
+
+    return HttpResponse(serialize("json", reviews), status=200)
+
+
+def fetch_reviews_by_store(request: HttpRequest, store_id: int):
+    store = Store.objects.get(pk=store_id)
+    products = Product.objects.filter(store=store)
+    reviews = UserReview.objects.filter(product=products)
+    return HttpResponse(serialize("json", reviews), status=200)
+
+
 def fetch_all_reviews(request) -> HttpResponse:
     reviews = UserReview.objects.all()
-    context = {"reviews": reviews}
-    return render(request, "test.html", context)
+    return HttpResponse(serialize("json", reviews), status=200)
 
 
 @require_POST
@@ -43,7 +63,6 @@ def add_comment(request: HttpRequest, id: uuid.UUID) -> HttpResponse:
 
     The function returns
     """
-    review: UserReview = None
     response = HttpResponse()
 
     try:
