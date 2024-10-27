5305ca7ee5d02bfffffc5829d3f6def1ab08ded2
Meinhard Christian <gdmeinhard@gmail.com>
2024-10-27 21:42:07 +0700

    Added store favorite template

--- favorites/views.py
+++ favorites/views.py
@@ -52,3 +52,8 @@ def get_favorite_item_ids(request):
     favorites, created = FavoriteProductList.objects.get_or_create(user=request.user)
     ids = list(favorites.items.values_list('product', flat=True).distinct())
     return JsonResponse({"ids" : ids})
+
+def get_favorite_store_ids(request):
+    favorites, created = FavoriteStoreList.objects.get_or_create(user=request.user)
+    ids = list(favorites.items.values_list('store', flat=True).distinct())
+    return JsonResponse({"ids" : ids})
--- static/js/favorite_store.js
+++ static/js/favorite_store.js
@@ -1,29 +1,29 @@
 document.addEventListener("DOMContentLoaded", function() {
-    fetch("/favorites/get-favorite-ids")  
+    fetch("/favorites/get-favorite-store-ids")  
     .then(response => response.json())
     .then(data => {
         let favoritedIds = data.ids;  
-        $('.toggle-favorite-button').each(function () {
+        $('.toggle-favorite-store').each(function () {
             const button = $(this);
-            const productId = button.data('product-id'); 
+            const storeId = button.data('store-id'); 
             const svg = button.find('svg');
 
-            if (favoritedIds.includes(productId)) {
+            if (favoritedIds.includes(storeId)) {
                 svg.attr('fill', 'red');
             } else {
                 svg.attr('fill', 'none');
             }
         });
-        $('.toggle-favorite-button').on('click', function() {
+        $('.toggle-favorite-store').on('click', function() {
             const button = $(this);
-            const productId = button.data('product-id'); 
+            const storeId = button.data('store-id'); 
             const svg = button.find('svg');       
             const csrfToken = button.data('csrf');
             $.ajax({
                 type: 'POST',
-                url: '/favorites/toggle-product', 
+                url: '/favorites/toggle-store', 
                 data: {
-                    'product_id': productId,
+                    'store_id': storeId,
                     'csrfmiddlewaretoken': csrfToken
                 }
             }).then(function(response){
@@ -40,6 +40,6 @@ document.addEventListener("DOMContentLoaded", function() {
     }).catch(error => console.error('Error fetching favorite IDs:', error));
 });
 
-// For the button --> {% include 'favorite_button.html' with id={product.id} csrf_token=csrf_token %}
-// For referencing this <script src="{% static 'js/favorite.js' %}"></script>
+// For the button --> {% include 'favorite_store.html' with id={store.id} csrf_token=csrf_token %}
+// For referencing this <script src="{% static 'js/favorite_store.js' %}"></script>
 // Requires <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
