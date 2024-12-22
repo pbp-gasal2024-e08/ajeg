d804a5c398dc0ce015d18d7ce4e1642f99a7553c
andreast12 <andreast12.id@gmail.com>
2024-12-22 15:42:29 +0700

    feat: add announcement views for flutter

--- announcement/views.py
+++ announcement/views.py
@@ -8,7 +8,8 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.core import serializers
 from .forms import AnnouncementForm
 from django.urls import reverse
-
+import json
+from django.http import JsonResponse
 
 # Create your views here.
 @csrf_exempt
@@ -68,3 +69,45 @@ def delete_announcement(request, id):
     return HttpResponseRedirect(
         reverse("checkout:store_page", args=[announcement.store.id])
     )
+
+@csrf_exempt
+def create_announcement_flutter(request):
+    if request.method == 'POST':
+
+        data = json.loads(request.body)
+
+        new_announcement = Announcement.objects.create(
+            title=data["title"],
+            description=data["description"],
+            store_id=int(data["store"]),
+        )
+
+        new_announcement.save()
+
+        return JsonResponse({"status": "success"}, status=200)
+    else:
+        return JsonResponse({"status": "error"}, status=401)
+
+@csrf_exempt
+def edit_announcement_flutter(request, id):
+    if request.method == 'POST':
+        data = json.loads(request.body)
+        
+        try:
+            announcement = Announcement.objects.get(pk=id)
+        except Announcement.DoesNotExist:
+            return JsonResponse({"status": "error", "message": "Announcement not found"}, status=404)
+
+        announcement.title = data["title"]
+        announcement.description = data["description"]
+        announcement.save()
+
+        return JsonResponse({"status": "success"}, status=200)
+    else:
+        return JsonResponse({"status": "error"}, status=401)
+
+@csrf_exempt
+def delete_announcement_flutter(request, id):
+    announcement = Announcement.objects.get(pk=id)
+    announcement.delete()
+    return JsonResponse({"status": "success"}, status=200)
